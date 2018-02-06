9c7c61e9ac31bec5bfa16d31fbdc457f10f802e6
Kylta <Christophe.bugnon02@gmail.com>
2018-02-06 11:56:18 +0100

    Connect storyboard to  PictureView

--- Instagrid/Instagrid/View/PictureView.swift
+++ Instagrid/Instagrid/View/PictureView.swift
@@ -10,12 +10,7 @@ import UIKit
 
 class PictureView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var stackTopView: UIStackView!
+    @IBOutlet weak var stackBottomView: UIStackView!
+    
 }
