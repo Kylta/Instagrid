26911d3fcc12ed61f07b37db25b39c2ff38434ab
Kylta <Christophe.bugnon02@gmail.com>
2018-02-06 11:50:36 +0100

    Create class PictureView

---
+++ Instagrid/Instagrid/View/PictureView.swift
@@ -0,0 +1,21 @@
+//
+//  PictureView.swift
+//  Instagrid
+//
+//  Created by Christophe Bugnon on 06/02/2018.
+//  Copyright © 2018 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class PictureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
