9719718c04004e1a1f8c5d6675e66faf1fa0445f
Kylta <Christophe.bugnon02@gmail.com>
2018-02-06 16:09:44 +0100

    Rename method didTapButton with didTapBottomForChooseOrder and add comments for methods

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     private func swipeUpToShare() {
+        // A concrete subclass of UIGestureRecognizer that looks for swiping gestures in one or more directions.
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
         swipeUp.direction = UISwipeGestureRecognizerDirection.up
         self.view.addGestureRecognizer(swipeUp)
@@ -38,15 +39,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            
+            // Creates a bitmap-based graphics context and makes it the current context.
             UIGraphicsBeginImageContext(pictureView.frame.size)
-            
+            // Renders the layer and its sublayers into the specified context.
             pictureView.layer.render(in: UIGraphicsGetCurrentContext()!)
-            
+            // Returns an image based on the contents of the current bitmap-based graphics context.
             guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return }
-            
+            // The permitted direction of the swipe for this gesture recognizer.
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.up:
+                // A view controller that you can use to offer various services from your app.
                 let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
                 
                 self.present(activityVC, animated: true, completion: nil)
@@ -59,13 +61,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // MARK: - @IBaction Methods
 
-    @IBAction func didTapButton(_ sender: UIButton) {
+    @IBAction func didTapBottomButtonForChooseOrder(_ sender: UIButton) {
         switch sender.tag {
         case 0:
+            // left button
             setImage(leftTopPictureIsHidden: true, leftBottomPictureIsHidden: false)
         case 1:
+            // middle button
             setImage(leftTopPictureIsHidden: false, leftBottomPictureIsHidden: true)
         case 2:
+            // right button
             setImage(leftTopPictureIsHidden: false, leftBottomPictureIsHidden: false)
         default:
             break
@@ -73,12 +78,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func choosePicture(_ sender: UIButton) {
+        // An object that manages customizable, system-supplied user interfaces for taking pictures and movies on supported devices, and for choosing saved images and movies for use in your app.
         let imagePickerController = UIImagePickerController()
+        // The image picker’s delegate object.
         imagePickerController.delegate = self
-        
+        // An object that displays an alert message to the user.
         let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
-        
+        // Attaches an action object to the alert or action sheet.
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+            // The type of picker interface to be displayed by the controller.
             imagePickerController.sourceType = .camera
             self.imagePickedController = sender.tag
             self.present(imagePickerController, animated: true, completion: nil)
@@ -98,12 +106,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // MARK: - UIImagePickerControllerDelegate Methods
     
+    // Tells the delegate that the user picked a still image or movie.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             pictureView.picture[imagePickedController].setImage(pickedImage, for: .normal)
             pictureView.picture[imagePickedController].contentMode = .scaleAspectFit
         }
         
+        // Dismisses the view controller that was presented modally by the view controller.
         picker.dismiss(animated: true, completion: nil)
     }
     
